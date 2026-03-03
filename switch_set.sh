64c7b75b1ab902dc45a2a016ea467f66be9560e9
Ivan  Miziuk <i1miziuk@gmail.com>
2026-03-02 18:02:25 -0800

    bach command to set swiches

--- switch_set.sh
+++ switch_set.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the switch IP addresses
-SWITCHES=("10.12.100.9" "10.12.100.10" "10.12.100.11")
-PORTS=("2006" "2004" "2006")
+SWITCHES=("10.12.100.13" "10.12.100.13" "10.12.100.13")
+PORTS=("2003" "2004" "2011")
 PASSWORD="chico"
 
 for i in "${!SWITCHES[@]}"; do
@@ -19,41 +19,32 @@ for i in "${!SWITCHES[@]}"; do
     # We use a Here-Doc (<< EOF) to pass the Expect script directly from Bash
     /usr/bin/expect << EOF
     
-    # Set a timeout (in seconds) so the script doesn't hang forever if a switch is dead
     set timeout 10
     
-    # Start the telnet session
     spawn telnet $IP $PORT
 
-    # Send a carriage return to wake up the console line
     send "\r"
 
-    # This block dynamically handles the login and privilege escalation
     expect {
-        -re "Password:" {
-            # If it asks for a password, send it and loop back
+        -re ":" {
             send "$PASSWORD\r"
             exp_continue
         }
         -re ">" {
-            # If in User EXEC mode, go to Privileged mode and loop back
             send "enable\r"
             exp_continue
         }
         -re "#" {
-            # If in Privileged EXEC mode, we are ready to configure
             send "configure terminal\r"
         }
         timeout {
-            # If nothing happens, press Enter again to force a prompt
             send "\r"
             exp_continue
         }
     }
 
-    # Now we rely strictly on the configuration prompts before sending the next command
     expect "(config)#"
-    send "spanning-tree mode rapid-pvst\r"    
+    send "spanning-tree mode pvst\r"    
 
     expect "(config)#"
     send "end\r"
@@ -64,7 +55,6 @@ for i in "${!SWITCHES[@]}"; do
     expect "#"
     send "exit\r"
     
-    # Close the expect session
     expect eof
 EOF
 
